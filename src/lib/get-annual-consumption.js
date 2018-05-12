5b00b08e3aa8f66b6077db5e5d7249446d97b867
Laurie Boyes <laurie.boyes@ft.com>
2018-05-12 09:01:19 +0100

    do the usage calculation

--- src/lib/get-annual-consumption.js
+++ src/lib/get-annual-consumption.js
@@ -1,6 +1,36 @@
+const toTwoDecimalPlaces = require('./util/to-two-decimal-places');
 const prices = require('../config/prices.json');
+const vatMultiplier = require('../config/vat-multiplier.json');
 
-module.exports = (tariffName, fuelType, targetMonthlySpend) => {
-	console.log('prices', prices);
-	return 'lol';
+module.exports = ({ tariffName, fuelType, targetMonthlySpend }) => {
+
+	// I recognise the irony of deciding not to use TypeScript in favour of all of these manual type checks
+	if (typeof tariffName !== 'string' || !tariffName.length) {
+		throw new Error(`Invalid tarrif name provided: ${tariffName}`);
+	}
+	if (typeof fuelType !== 'string' || !fuelType.length) {
+		throw new Error(`Invalid fuel type provided: ${fuelType}`);
+	}
+	if (typeof targetMonthlySpend !== 'number') {
+		throw new Error(`Invalid target monthly spend provided: ${typeof targetMonthlySpend} ${targetMonthlySpend}`);
+	}
+
+	const tariffPrice = prices.find(p => p.tariff === tariffName)
+	if (!tariffPrice) {
+		throw new Error(`No tarrif found with name ${tariffName}`);
+	}
+
+	const standingCharge = tariffPrice.standing_charge;
+
+	const ratePerKwh = tariffPrice.rates[fuelType];
+	if (!ratePerKwh) {
+		throw new Error(`Invalid fuel type ${fuelType} for tarrif with name ${tariffName}`);
+	}
+
+	const yearlySpendMinusStandingCharge = (targetMonthlySpend - standingCharge) * 12;
+	const kwhAnnually = yearlySpendMinusStandingCharge / ratePerKwh;
+
+	const kwhAnnuallyIncludingVat = kwhAnnually * vatMultiplier;
+
+	return toTwoDecimalPlaces(kwhAnnuallyIncludingVat);
 }
--- test/lib/get-annual-consumption.spec.js
+++ test/lib/get-annual-consumption.spec.js
@@ -8,12 +8,12 @@ const mockPrices = [
 		"tariff": "some-energy",
 		"rates": {
 			"power": 0.1367,
-			"gas": 0.0288
+			"gas": 0.0288 // fee per kilowatt hour
 		},
 		"standing_charge": 8.33
 	},
 	{
-		"tariff": "some-other-energy",
+		"tariff": "some-no-standing-charge-energy",
 		"rates": {
 			"power": 0.1397,
 			"gas": 0.0296
@@ -22,14 +22,21 @@ const mockPrices = [
 	}
 ]
 
+// Possibly edging _slightly_ towards overkill by mocking VAT as well but I think it's worthwhile
+const mockVatMultiplier = 1.1;
+
 const getAnnualConsumption = proxyquire('../../src/lib/get-annual-consumption', {
-	'../config/prices.json': mockPrices
+	'../config/prices.json': mockPrices,
+	'../config/vat-multiplier.json': mockVatMultiplier,
 });
 
 describe('getAnnualConsumption()', () => {
-
-	it('should do a thing', () => {
-		expect(getAnnualConsumption())
-			.to.equal('lol');
+	it('should return the total annual consumption in kWh, inclusive of VAT, rounded to two decimal places', () => {
+		expect(getAnnualConsumption({
+			tariffName: 'some-energy',
+			fuelType: 'gas',
+			targetMonthlySpend: 40
+		}))
+			.to.equal(14515.42);
 	});
 });
